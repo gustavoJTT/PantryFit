a311e1a77d1284e14830aa0366efb1cac0cef237
Gustavo Tavares <gustavojosetata@gmail.com>
2026-09-10 22:37:24 -0300

    chore(PANTRY-005): Add CI pipeline

--- src/test/java/br/com/pantryfit/PantryFitApplicationTests.java
+++ src/test/java/br/com/pantryfit/PantryFitApplicationTests.java
@@ -1,13 +1,19 @@
 package br.com.pantryfit;
 
 import org.junit.jupiter.api.Test;
+import org.springframework.context.ApplicationContext;
 import org.springframework.boot.test.context.SpringBootTest;
+import org.springframework.test.context.ActiveProfiles;
+
+import static org.junit.jupiter.api.Assertions.assertNotNull;
 
 @SpringBootTest
+@ActiveProfiles("test")
 class PantryFitApplicationTests {
 
 	@Test
-	void contextLoads() {
+	void contextLoads(ApplicationContext applicationContext) {
+		assertNotNull(applicationContext);
 	}
 
 }
